2212a052e09d9294830c9c342cb4cb5aab3d6e4b
frederic-hallein <frederic.hallein@outlook.com>
2024-12-02 15:40:41 +0100

    finished add and division test

--- src/Calculator.cs
+++ src/Calculator.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using System.Security.Principal;
 
 namespace CalculatorNS
@@ -5,5 +6,8 @@ namespace CalculatorNS
     public class Calculator
     {
         public int Add(int a, int b) { return a + b; }
+
+        public float Divide(float a, float b) { return a / b; }
+
     }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-using CalculatorNS;
+﻿using CalculatorNS;
 
 namespace MainNS
 {
@@ -8,12 +6,17 @@ namespace MainNS
     {
         public static void Main()
         {
-            Console.WriteLine("hello world");
-            int a = 1;
+            int a = 0;
             int b = 2;
             var calculator = new Calculator();
             int sum = calculator.Add(a, b);
             Console.WriteLine($"{a} + {b} = {sum}");
+
+
+            int c = 0;
+            float quotient = calculator.Divide(a, c);
+            Console.WriteLine($"{a} / {c} = {quotient}");
+
         }
     }
 }
